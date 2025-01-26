f76a259c05533aebb8ee609938b6a7a604bbfd89
idknevo <nevoexists@gmail.com>
2025-01-26 04:30:27 +0200

    managed files structure and added routes

---
+++ src/routes.jsx
@@ -0,0 +1,38 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home.jsx";
+import Menu, { loader as menuLoader } from "./features/menu/Menu.jsx";
+import Cart from "./features/cart/Cart.jsx";
+import CreateOrder from "./features/order/CreateOrder.jsx";
+import Order from "./features/order/Order.jsx";
+import AppLayout from "./ui/AppLayout.jsx";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+      },
+    ],
+  },
+]);
+
+export default router;
